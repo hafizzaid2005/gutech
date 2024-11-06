3b21648d89caca77c98a323c26843e269f276e7f
99ahsanahmed <ahsan.ha58@gmail.com>
2024-11-06 01:26:22 -0800

    week 4

---
+++ WT_lab-04_tasks_JSpt2/script.js
@@ -0,0 +1,33 @@
+// task1
+let naam = "Ahsan"
+document.getElementById("task1").innerText = `Hello ${naam} !`
+
+// task2
+let num1 = 4;
+let num2 = 10;
+let sum = num1 + num2;
+document.getElementById("task2").innerText = `Sum of ${num1} amd ${num2} is  ${sum} `
+
+// task3
+let celcius = 90;
+let farenhiet = ( celcius * 9/5) + 32
+document.getElementById("task3").innerText = `${celcius} celcius into farenhiet is = ${farenhiet}`
+
+
+//task4
+let byear = 2005
+let cyear = 2024
+let age = cyear - byear;
+document.getElementById("task4").innerText = ` Borned in ${byear} and current year is ${cyear} it means your age is ${age}  `
+
+//task5
+let numOne = 5;
+let numTwo = 15;
+let product = numOne * numTwo
+document.getElementById("task5").innerText = `Number one is ${numOne} Number one is ${numOne} Number two is ${numTwo} and product is = ${product} `
+
+//task6
+let width = 5;
+let height = 15;
+let area = width * height;
+document.getElementById("task6").innerText = `area is ${area}`
